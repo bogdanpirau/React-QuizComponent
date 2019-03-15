1730f26aaa0d78dcdd951b0c86cf6e9f0d4de0a9
Pirau Bogdan <bogdanp@Cliotech.com>
2019-03-15 12:14:34 +0200

    Added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 import Quiz from './Quiz'
+import QuizAdd from './QuizAdd';
 
 class App extends Component {
   render() {
     return (
-      <Quiz />
-    )
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Quiz</Link>
+            </li>
+            <li>
+              <Link to="/Add">Add question</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Quiz} />
+          <Route path="/Add" component={QuizAdd} />
+        </div>
+      </Router>
+    );
   }
 }
 
-export default App
+export default App;
