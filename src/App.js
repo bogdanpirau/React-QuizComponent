570d23006f3dfc30c95277bb3aae5a0e0d7aeb80
Pirau Bogdan <bogdanp@Cliotech.com>
2019-03-22 16:39:07 +0200

    Added mobX and wrapped navigation into Provider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider } from "mobx-react"
 import './App.css'
 import Quiz from './Quiz'
 import QuizAdd from './QuizAdd';
 
 class App extends Component {
+  store = {};
+
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Quiz</Link>
-            </li>
-            <li>
-              <Link to="/Add">Add question</Link>
-            </li>
-          </ul>
+        <Provider store={this.store}>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Quiz</Link>
+              </li>
+              <li>
+                <Link to="/Add">Add question</Link>
+              </li>
+            </ul>
 
-          <hr />
+            <hr />
 
-          <Route exact path="/" component={Quiz} />
-          <Route path="/Add" component={QuizAdd} />
-        </div>
+            <Route exact path="/" component={Quiz} />
+            <Route path="/Add" component={QuizAdd} />
+          </div>
+        </Provider>
       </Router>
     );
   }
